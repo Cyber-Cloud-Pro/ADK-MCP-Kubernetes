c55f3dfeb41d9f700e888099dd39fbc897d4bbdc
Cyber-Cloud-Pro <atharva.dalvi.yt@gmail.com>
2025-05-08 14:48:44 +0530

    Update req.sh

--- req.sh
+++ req.sh
@@ -31,12 +31,4 @@ echo "==== Installing kubectl ===="
 sudo snap install kubectl --classic
 echo "kubectl version: $(kubectl version --client --short)"
 
-echo " Creating a Python Virtual Env "
-sudo apt install python3.12-venv -y
-python3 -m venv .venv
-source .venv/bin/activate
-echo "-----Installing the requirements -----"
-pip install -r req.txt
 
-source .venv/bin/activate
-adk web
