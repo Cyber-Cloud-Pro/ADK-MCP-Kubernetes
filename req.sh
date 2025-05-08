8ae7e0db85d941f07ee8a90d57aa1b253f00cc74
Cyber-Cloud-Pro <atharva.dalvi.yt@gmail.com>
2025-05-08 15:41:50 +0530

    Update req.sh

--- req.sh
+++ req.sh
@@ -29,8 +29,6 @@ echo "starting minikube "
 echo "==== Installing kubectl ===="
 sudo snap install kubectl --classic
 echo "kubectl version: $(kubectl version --client --short)"
-sudo su 
-exit
 
 
 
